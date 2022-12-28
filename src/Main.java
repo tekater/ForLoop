5031edae7cb57c7504acb1547aedfd5dda7b4872
tekater <120014627+tekater@users.noreply.github.com>
2022-12-28 21:43:22 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,16 @@ public class Main {
             }
             System.out.print(i + " ");
         }
+        // Оператор ветвлени break
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(j + " ");
+                if (i == j) {
+                    break;
+                }
+
+            }
+            System.out.println();
+        }
     }
 }
